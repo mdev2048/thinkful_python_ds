ce71181f3b7ad68780697921893a28e36239d520
mdev2048 <mdev2048@users.noreply.github.com>
2014-12-17 11:50:33 -0500

    chiSquare update

--- U2L2S3/chi_squared.py
+++ U2L2S3/chi_squared.py
@@ -8,12 +8,17 @@ loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loan
 # Drop null rows
 loansData.dropna(inplace=True)
 
+def chiSquare(observedCnt, expectedCnt):
+	return (observedCnt - expectedCnt) * (observedCnt - expectedCnt) / expectedCnt
+
 freq = collections.Counter(loansData['Open.CREDIT.Lines'])
 print "# of distinct number of credit lines", len(freq.keys())
 print "total credit lines", sum(freq.values())
-print "expected count for each number of credit lines", sum(freq.values()) / float(len(freq.keys()))
+expectedCnt =  sum(freq.values()) / float(len(freq.keys()))
+print "expected count for each number of credit lines", expectedCnt
 
-
+for i in range(0,len(freq.keys())):
+	print freq.keys()[i], " : ", chiSquare(freq.values()[i],expectedCnt)
 plt.figure()
 plt.bar(freq.keys(), freq.values(), width=1)
 plt.ylabel("Count")
